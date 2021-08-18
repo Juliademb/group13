d1c0c04e12709179cbd2afd61f606828a89fbbf3
yuliasemenyuk <semenyukyp@gmail.com>
2021-08-19 01:08:41 +0300

    Update main-hero-anchor.js

--- src/js/main-hero-anchor.js
+++ src/js/main-hero-anchor.js
@@ -1,10 +1,14 @@
-const anchors = document.querySelectorAll('a[href*="#"]')
+// Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]')
 
-for (let anchor of anchors) {
-  anchor addEventListener("click", function(event) {
-    event.preventDefault();
-    const blockID = anchor.getAttribute('href')
-    document.querySelector('' + blockID).scrollIntoView({
+// Цикл по всем ссылкам
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
       behavior: "smooth",
       block: "start"
     })
