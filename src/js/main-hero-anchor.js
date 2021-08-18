856c77805192751ffb315679fd5143bc2c3ee28e
yuliasemenyuk <semenyukyp@gmail.com>
2021-08-19 00:30:07 +0300

    anchor

---
+++ src/js/main-hero-anchor.js
@@ -0,0 +1,5 @@
+$("body").on('click', '[href*="#"]', function(e){
+    var fixed_offset = 100;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+  });
